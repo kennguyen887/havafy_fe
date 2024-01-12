2011f1416c93fb9618bc107f46be15e739003dc1
kennguyen.care <ngu.nt@carehealth.io>
2024-01-12 19:23:49 +0700

    feat: update script gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,21 +14,18 @@ class MyDocument extends Document {
   }
 
   render() {
-    const gTagID = process.env.NEXT_PUBLIC_GTAG || '';
+    const GTM_ID = process.env.NEXT_PUBLIC_GTAG || '';
     return (
       <Html lang='en'>
         <Head>
-          <Script
-            src={'https://www.googletagmanager.com/gtag/js?id=' + gTagID}
-          />
-          <Script id='google-analytics'>
+          <Script id='google-tag-manager' strategy='afterInteractive'>
             {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', '${gTagID}');
-          `}
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `}
           </Script>
           <link
             rel='preload'
