0cb6328fcff9507cb1f9cca670d835ff0258d75f
kennguyen.care <ngu.nt@carehealth.io>
2024-01-24 17:19:35 +0700

    feat: build order form

--- src/components/checkout/CheckoutForm.tsx
+++ src/components/checkout/CheckoutForm.tsx
@@ -13,17 +13,23 @@ import TextInput from '@/components/form/TextInput';
 
 import { useAuthState } from '@/contexts/AuthContext';
 import { GetOrdeItemResDto } from '@/domain/dto';
+function CheckoutFormSubmitted({ orderId }: { orderId: string }) {
+  return (
+    <div className='mb-5 bg-white p-5'>
+      <h3>Created order</h3>
+      {orderId}
+    </div>
+  );
+}
 
 export default function CheckoutForm() {
   const searchParams = useSearchParams();
   const [promoCode, setPromoCode] = useState<string | null>(null);
-  // eslint-disable-next-line unused-imports/no-unused-vars
-  const [promoDiscount, setPromoDiscount] = useState<number>();
   const [subtotal, setSubtotal] = useState<number>();
   const [discountTotal, setDiscountTotal] = useState<number>();
   const [grandTotal, setGrandTotal] = useState<number | null>(null);
+  const [orderId, setOrderId] = useState<string>();
   const [items, setItems] = useState<GetOrdeItemResDto[]>();
-  // eslint-disable-next-line unused-imports/no-unused-vars
   const [alert, setAlert] = React.useState<string>();
   const [haveCoupon, setHaveCoupon] = React.useState<boolean>(false);
   const { isAuthenticated } = useAuthState();
@@ -38,7 +44,6 @@ export default function CheckoutForm() {
         };
       }),
     }).then((data) => {
-      setPromoDiscount(data.promoDiscount);
       setSubtotal(data.subtotal);
       setDiscountTotal(data.discountTotal);
       setGrandTotal(data.grandTotal);
@@ -78,118 +83,131 @@ export default function CheckoutForm() {
         };
       }),
     }).then((data) => {
-      setPromoDiscount(data.promoDiscount);
+      if (data.statusCode) {
+        setAlert(data.message);
+        return;
+      }
+
+      if (data.orderId) {
+        setOrderId(data.orderId);
+      }
     });
   };
   // onFinish={(values) => handleReCaptchaVerify(values)}
 
   return (
     <div className='max-w-24'>
-      <form noValidate>
-        <div className='mb-5 bg-white p-5'>
-          <Alert content={alert} hidden={!alert} />
-          <div id='cartItems' className='w-96 border-b border-gray-300 text-sm'>
-            {items?.map((item) => {
-              return (
-                <div className='flex flex-row' key={item.sku}>
-                  <div className='basis-2/3 px-2 py-3 font-semibold'>
-                    {item.name}
+      {orderId ? (
+        <CheckoutFormSubmitted orderId={orderId} />
+      ) : (
+        <form noValidate>
+          <div className='mb-5 bg-white p-5'>
+            <Alert content={alert} hidden={!alert} />
+            <div
+              id='cartItems'
+              className='w-96 border-b border-gray-300 text-sm'
+            >
+              {items?.map((item) => {
+                return (
+                  <div className='flex flex-row' key={item.sku}>
+                    <div className='basis-2/3 px-2 py-3 font-semibold'>
+                      {item.name}
+                    </div>
+                    <div className='basis-1/3 px-2 py-3 text-right font-semibold'>
+                      US${item.total}
+                    </div>
                   </div>
-                  <div className='basis-1/3 px-2 py-3 text-right font-semibold'>
-                    US${item.total}
-                  </div>
+                );
+              })}
+            </div>
+            <div
+              id='cartTotal'
+              className='border-b border-gray-300 py-2 text-xs '
+            >
+              <div className='flex flex-row'>
+                <div className='basis-2/3 px-2 py-2'>Subtotal</div>
+                <div className='basis-1/3 px-2 py-2 text-right'>
+                  US${subtotal}
                 </div>
-              );
-            })}
-          </div>
-          <div
-            id='cartTotal'
-            className='border-b border-gray-300 py-2 text-xs '
-          >
-            <div className='flex flex-row'>
-              <div className='basis-2/3 px-2 py-2'>Subtotal</div>
-              <div className='basis-1/3 px-2 py-2 text-right'>
-                US${subtotal}
+              </div>
+              <div className='flex flex-row'>
+                <div className='basis-2/3 px-2 py-2'>Discount</div>
+                <div className='basis-1/3 px-2 py-2 text-right'>
+                  US${discountTotal}
+                </div>
               </div>
             </div>
-            <div className='flex flex-row'>
-              <div className='basis-2/3 px-2 py-2'>Discount</div>
-              <div className='basis-1/3 px-2 py-2 text-right'>
-                US${discountTotal}
+            <div id='grandTotal' className='font-semibold'>
+              <div className='flex flex-row'>
+                <div className='basis-2/3 px-2 py-3'>Total</div>
+                <div className='basis-1/3 px-2 py-3 text-right'>
+                  US${grandTotal}
+                </div>
               </div>
             </div>
-          </div>
-          <div id='grandTotal' className='font-semibold'>
-            <div className='flex flex-row'>
-              <div className='basis-2/3 px-2 py-3'>Total</div>
-              <div className='basis-1/3 px-2 py-3 text-right'>
-                US${grandTotal}
+            <div id='couponApply'>
+              <div
+                className={clsx(
+                  haveCoupon ? 'hidden' : '',
+                  'mt-2 cursor-pointer text-xs text-blue-500'
+                )}
+                onClick={() => setHaveCoupon(true)}
+              >
+                Have a Coupon?
               </div>
-            </div>
-          </div>
-          <div id='couponApply'>
-            <div
-              className={clsx(
-                haveCoupon ? 'hidden' : '',
-                'mt-2 cursor-pointer text-xs text-blue-500'
-              )}
-              onClick={() => setHaveCoupon(true)}
-            >
-              Have a Coupon?
-            </div>
-            <div
-              className={clsx(
-                haveCoupon ? '' : 'hidden',
-                'flex flex-row border-t border-gray-300 pt-5'
-              )}
-            >
-              <div className='mr-5 basis-3/4'>
-                <TextInput
-                  name='Enter your coupon'
-                  id='coupon'
-                  className='h-10'
-                  currentValue={(value) => setPromoCode(value)}
-                />
-              </div>
-              <div className='basis-1/4 text-right'>
-                <PrimaryButton
-                  className='h-10'
-                  name='Apply'
-                  onClick={() => getOrderBySkuList(skuList)}
-                />
+              <div
+                className={clsx(
+                  haveCoupon ? '' : 'hidden',
+                  'flex flex-row border-t border-gray-300 pt-5'
+                )}
+              >
+                <div className='mr-5 basis-3/4'>
+                  <TextInput
+                    name='Enter your coupon'
+                    id='coupon'
+                    className='h-10'
+                    currentValue={(value) => setPromoCode(value)}
+                  />
+                </div>
+                <div className='basis-1/4 text-right'>
+                  <PrimaryButton
+                    className='h-10'
+                    name='Apply'
+                    onClick={() => getOrderBySkuList(skuList)}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        {typeof grandTotal === 'number' && grandTotal > 0 ? (
-          <PayPalButton
-            options={{
-              clientId:
-                'AV8-_8dTFiq9NwHnXct-Fdwxhwhn0QP6Ph8KfmwmXR6Aqspj7wXWAkIY9O4Q6iNM6VBkh69xIP2AuQe0',
-            }}
-            amount={grandTotal}
-            // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onSuccess={(details: any, data: any) => {
-              // eslint-disable-next-line no-console
-              console.log('-----------details', details);
-              // eslint-disable-next-line no-console
-              console.log('-----------data', data);
-              // OPTIONAL: Call your server to save the transaction
-              return submitForm({
-                paymentMethod: 'paypal',
-                paymentOrderId: data.orderID,
-              });
-            }}
-          />
-        ) : (
-          <FlatButton
-            name='Checkout'
-            className='w-full'
-            onClick={() => submitForm}
-          />
-        )}
-      </form>
+          {typeof grandTotal === 'number' && grandTotal > 0 ? (
+            <PayPalButton
+              options={{
+                clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+              }}
+              amount={grandTotal}
+              // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              onSuccess={(details: any, data: any) => {
+                // eslint-disable-next-line no-console
+                console.log('-----------details', details);
+                // eslint-disable-next-line no-console
+                console.log('-----------data', data);
+                // OPTIONAL: Call your server to save the transaction
+                return submitForm({
+                  paymentMethod: 'paypal',
+                  paymentOrderId: data.orderID,
+                });
+              }}
+            />
+          ) : (
+            <FlatButton
+              name='Checkout'
+              className='w-full'
+              onClick={() => submitForm}
+            />
+          )}
+        </form>
+      )}
     </div>
   );
 }
